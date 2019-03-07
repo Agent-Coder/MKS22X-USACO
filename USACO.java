c4d35144c055a878115701499a150d3b2d07578a
Amanda Zheng <azheng00@stuy.edu>
2019-03-07 10:41:02 -0500

    works for one case

--- USACO.java
+++ USACO.java
@@ -8,7 +8,7 @@ public class USACO{
   private static int[][] map;
   private static int[] instructions;
   private static void bronzeFile(String filename)throws FileNotFoundException{
-    File text = new File("Maze.txt");
+    File text = new File(filename);
     Scanner inf = new Scanner(text);
     String[] one=inf.nextLine().split(" ",-1);
     R=Integer.parseInt(one[0]);
@@ -16,31 +16,39 @@ public class USACO{
     E=Integer.parseInt(one[2]);
     N=Integer.parseInt(one[3]);
     map=new int[R][C];
+    instructions= new int[3*N];
     for (int x=0;x<R;x++) {
       one=inf.nextLine().split(" ",-1);
       for (int y=0;y<C;y++ ) {
         map[x][y]=Integer.parseInt(one[y]);
       }
     }
+    int b=0;
     while(inf.hasNextLine()){
       one = inf.nextLine().split(" ",-1);
-      for (int a=0;a<C;a++) {
-        instructions[a]=Integer.parseInt(one[a]);
+      for (int a=0;a<3;a++) {
+        instructions[3*b+a]=Integer.parseInt(one[a]);
+        //System.out.println(instructions[a]);
       }
+      b++;
     }
   }
   private static void stomp(int r, int c, int e){
-    int highest=0;;
-    for (int a=r;a<r+3 ;a++ ) {
-      for (int b=c;b<c+3 ;b++ ) {
+    int highest=0;
+    //System.out.println(r+" "+c);
+    for (int a=r-1;a<r+2;a++ ) {
+      for (int b=c-1;b<c+2;b++ ) {
         if(map[a][b]>highest){
           highest=map[a][b];
         }
       }
     }
     highest=highest-e;
-    for (int a=r;a<r+3 ;a++ ) {
-      for (int b=c;b<c+3 ;b++ ) {
+    //System.out.println(highest);
+    for (int a=r-1;a<r+2 ;a++ ) {
+      //System.out.print("\n");
+      for (int b=c-1;b<c+2 ;b++ ) {
+        //System.out.print(map[a][b]+" ");
         map[a][b]=highest;
       }
     }
@@ -48,15 +56,20 @@ public class USACO{
   }
   private static void depths(){
     for (int a=0;a<R ;a++ ) {
+      System.out.print("\n");
       for (int b=0;b<C ;b++ ) {
+        System.out.print(map[a][b]+" ");
           map[a][b]=E-map[a][b];
+
       }
     }
   }
   private static int sum(){
     int summation=0;
     for (int a=0;a<R ;a++ ) {
+
       for (int b=0;b<C ;b++ ) {
+
           if(map[a][b]>0){
             summation+=map[a][b];
           }
@@ -64,11 +77,22 @@ public class USACO{
     }
     return summation;
   }
-  public static int bronze(String filename){
+  public static int bronze(String filename)throws FileNotFoundException{
+    bronzeFile(filename);
     for (int i=0;i<instructions.length/3;i++ ) {
-      stomp(instructions[i],instructions[i+1],instructions[i+2]);
+      //System.out.println(instructions[3*i]+","+instructions[3*i+1]+","+instructions[3*i+2]);
+      stomp(instructions[3*i],instructions[3*i+1],instructions[3*i+2]);
     }
     depths();
+    //System.out.println(sum());
     return sum()*72*72;
+  }
+  public static void main(String[] args) {
+    try{
+    System.out.println(bronze("Sample.txt"));
+  }
+  catch(FileNotFoundException e){
+    System.out.println("file not found");
+  }
   }
 }
